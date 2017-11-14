016f93025457e121bcea74c90ba883d6c100f2e1
Justo van der Werf <justovdwerf@gmail.com>
2017-11-14 14:17:08 +0100

    met rendom

--- classtest.py
+++ classtest.py
@@ -2,8 +2,9 @@ import numpy as np
 import csv
 import pandas as pd
 import random
+import sys
 
-#constants
+# Define constants.
 BSIZE = 6
 VERTICAL = 1
 HORIZONTAL = 2
@@ -11,25 +12,23 @@ DOWN = 1
 RIGHT = 1
 UP = -1
 LEFT = -1
+EMPTY_SPACE = 0
 
-winningy = int((BSIZE - 1) / 2)
+# If car 1 is on the winning coordinates, the game is finshed.
 winningx = BSIZE - 1
+winningy = int((winningx) / 2)
 
+# Make two dimensional grit of zeros with the size BSIZE.
 board = np.zeros((BSIZE,BSIZE))
 
-spel = pd.read_csv("games/game1.csv", delimiter = "\t")
-print(type(spel.iloc[2]['orient']))
-print(len(spel.index))
+# Import the csv file of the game you want to play.
+spel = pd.read_csv("games/game2.csv", delimiter = "\t")
 
-
-# make function to place cars on the board
-# set coordinate and orintation
-# orientation 1 = vertical, 2 = horizontal
+# Initialize the board.
 class Car():
     "class to make cars"
 
     def __init__(self, car_id, y, x, orient, size):
-
         self.y = y
         self.x = x
         self.car_id = car_id
@@ -40,53 +39,53 @@ class Car():
         elif orient == HORIZONTAL:
             self.last = x + size
 
-        # Place car vertical
-        if orient == 1:
+        # Place every car according to orient, if not possible print error.
+        if orient == VERTICAL:
             board[y:size+y,x] = car_id
-
-        # Place car horizontal
-        elif orient == 2:
+        elif orient == HORIZONTAL:
             board[y,x:size+x] = car_id
-
         else:
             print("error")
 
+    # Function to check if car number 1 is on the winning coordinates.
     def check(self):
-        print(board)
-        print("******************************")
         if board[winningy][winningx] == 1:
             print("you won!")
+            sys.exit("WON")
+        return print(board, "\n")
 
-
+    # Function to move the cars by chaning the values on the board.
     def move(self, direction):
-
         for i in range(BSIZE):
             for j in range(BSIZE):
-                if board[i][j] == self.car_id :
-                    if self.orient == 1:
-                        if direction == 1 and board[i + self.size][j] == 0:
+                if board[i][j] == self.car_id:
+                    # Change values on board based on the orient and direction.
+                    if self.orient == VERTICAL:
+                        if direction == 1 and (i + self.size) < BSIZE and board[i + self.size][j] == 0:
+                            # Change x coordinate of the car with the direction.
                             board[i][j] = 0
                             Car(self.car_id, i + direction, j, self.orient, self.size)
-                            return self.check()
-                        elif direction == -1 and board[i + direction][j] == 0:
+                            self.check()
+                        elif direction == -1 and (i + direction) >= 0 and board[i + direction][j] == 0:
                             board[i+self.size-1][j] = 0
                             Car(self.car_id, i + direction, j, self.orient, self.size)
-                            return self.check()
+                            self.check()
                         else:
-                            print("invalid move")
-                            return None
-                    elif self.orient == 2:
-                        if direction == 1 and board[i][j + self.size] == 0:
+                            print("invalid move", "\n")
+                            return False
+                    else:
+                        if direction == 1 and (j + self.size) < BSIZE and board[i][j + self.size] == 0:
                             board[i][j] = 0
                             Car(self.car_id, i, j + direction, self.orient, self.size)
-                            return self.check()
-                        elif direction == -1 and board[i][j + direction] == 0:
+                            self.check()
+                        elif direction == -1 and (j + direction) >= 0 and board[i][j + direction] == 0:
                             board[i][j + self.size-1] = 0
                             Car(self.car_id, i, j + direction, self.orient, self.size)
-                            return self.check()
+                            self.check()
                         else:
-                            print("invalid move")
-                            return None
+                            print("invalid move", "\n")
+                            return False
+
 
 
 # examples how to place cars
@@ -97,14 +96,21 @@ for auto in range(len(spel.index)):
     cars.append(Car(int(spel.iloc[auto]['car_id']),int(spel.iloc[auto]['y']), int(spel.iloc[auto]['x']),
                         int(spel.iloc[auto]['orient']),int(spel.iloc[auto]['size'])))
 
-
-
-cars[1].move(DOWN)
-cars[6].move(UP)
-cars[2].move(LEFT)
+print(board)
+# cars[4].move(UP)
+# cars[1].move(LEFT)
 # cars[8].move(RIGHT)
-# cars[6].move(DOWN)
-
+# cars[6].move(UP)
+# cars[6].move(UP)
+# cars[6].move(UP)
+# cars[6].move(UP)
+# cars[6].move(UP)
+# cars[8].move(RIGHT)
+# print(board)
+# print()
+# car2.move(LEFT)
+# print(board)
+#
 
 def dirry():
     d = random.randint(1,2)
@@ -112,17 +118,22 @@ def dirry():
         d = -1
     return d
 
-for i in range(2000):
+for i in range(1000):
     r = random.randint(0,8)
     d = dirry()
-    print (r,d)
-    # cars[r].move(d)
+    while(cars[r].move(d) == True):
+        cars[r].move(d)
 
-# print(board)
-# print()
-# car2.move(LEFT)
-# print(board)
-#
+
+
+# Make function to move, where direction 1 = left/up and 2 = right/down
+# Step direction: +1 or -1
+# def move(car_id, stepdir):
+    # kijken of er niet al een auto staat, of het board niet ophoudt
+    # vertical geplaatste auto
+    # if orient == 1:
+        # board[]
+
 
 
 
