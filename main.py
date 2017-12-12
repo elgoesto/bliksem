9dd753c09ba6d90835e113a18a1b0ae175cb4849
[LotteNelson] <[nelson.lotte@kpnmail.nl]>
2017-12-12 14:57:48 +0100

    prompt user for input

--- main.py
+++ main.py
@@ -9,14 +9,29 @@ def main():
     # Initialize cars on the board.
     cars = cc.Board.makecars(cc.Car, cc.TOTAL_CARS, cc.spel)
 
-    print("test board met BFS")
-    bfs.BFS(cars, 5)
+    print("Choose 1 to run the random algorithm.")
+    print("Choose 2 to run the Bredth First Search algorithm.")
+    print("Choose 3 to run the Depth First Search algorithm.")
 
+    var = input("Please enter something: ")
+    print(var)
+
+    # print("test board met BFS")
+    # bfs.BFS(cars, 5)
+    #
     # print("")
-    #
-    # print ("test board met random")
-    # rand.randomize(cars, cc.RANDOM_CARS, cc.TOTAL_CARS)
-    #
+    if int(var) == 1:
+        print ("Test board with random algorithm.")
+        rand.randomize(cars, cc.RANDOM_CARS, cc.TOTAL_CARS)
+    elif int(var) == 2:
+        print("Test board with Breadth First Search.")
+        bfs.BFS(cars, 5)
+    elif int(var) == 3:
+        print("Test board with Depth First Search.")
+        dfs.DFS(cars, 5)
+    else:
+        print("Error, you did not enter one of these options.")
+
     # print("")
     #
     # print("test board met DFS")
